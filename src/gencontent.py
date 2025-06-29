1474d7b79a31d43422b95fc26e640e7f96952078
mcbk51 <marcopoulut@gmail.com>
2025-06-28 22:51:18 -0700

    almost complete

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,8 +1,20 @@
 import os
+from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path, basepath)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
+
+
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -18,6 +30,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', 'href="' + basepath)
+    template = template.replace('src="/', 'src="' + basepath)
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
@@ -33,21 +47,3 @@ def extract_title(md):
             return line[2:]
     raise ValueError("no title found")
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-    for root, _, files in os.walk(dir_path_content):
-        for filename in files:
-            if not filename.endswith("md."):
-                continue
-
-            from_path = os.path.join(root, filename)
-
-            rel_path = os.path.relpath(from_path, dir_path_content)
-            rel_dir = os.path.dirname(rel_path)
-
-            dest_dir = os.path.join(dest_dir_path, rel_dir)
-            os.makedirs(dest_dir,exist_ok=True)
-
-            output_filename = os.path.splittext(filename)[0] + ".html"
-            dest_path = os.path.join(dest_dir, output_filename)
-
-            generate_page(from_path,template_path,dest_path)
